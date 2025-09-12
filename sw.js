3588823eff158c0262a592ba9745483e461c574c
ፚ Ꭷ Ꮢ Ꭷ ❥ | 𝘽𝙇𝘼𝘾𝙆_𝙎𝙃𝘼𝙍𝙆 <101139382+ZORO2045@users.noreply.github.com>
2025-09-12 23:16:59 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-self.addEventListener('install', () => { self.skipWaiting(); });
-self.addEventListener('activate', () => { console.log('Service Worker active'); });
-self.addEventListener('fetch', (event) => {});
